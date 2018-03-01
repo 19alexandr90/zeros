76c4968002cf9145f2404760f612394f37467304
Alexandr Samusevich <36624664+19alexandr90@users.noreply.github.com>
2018-03-01 17:52:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  	let countZeros = 0;
+		while (number > 0) {
+				number = Math.floor( number / 5 );
+				countZeros = countZeros + number;
+		}
+		return countZeros;
 }
